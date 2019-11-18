df28900967489843adfc90bfe3edcca728dc4e05
Rustam Eynaliyev <rustameynaliyev@rustams-mbp.home>
2019-11-18 20:44:46 +0100

    update: import component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchFormComponent } from './pages/home/components/search-form/search-form.component';
+import { GyphListComponent } from './pages/home/components/gyph-list/gyph-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchFormComponent,
+    GyphListComponent
   ],
   imports: [
     BrowserModule,
