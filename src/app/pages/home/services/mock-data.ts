7a8d5e0fd9a361735a97a94270f6015d27cf74fe
Rustam Eynaliyev <rustameynaliyev@rustams-mbp.home>
2019-11-23 22:02:27 +0100

    fix mock data, minor ui fixes

--- src/app/pages/home/services/mock-data.ts
+++ src/app/pages/home/services/mock-data.ts
@@ -24,40 +24,21 @@ export const mockResponse: any = {
     {
       "type": "gif",
       "id": "3oKIPusxB2uXOmoP0k",
-      "url": "https://giphy.com/gifs/DNCE-3oKIPusxB2uXOmoP0k",
-      "slug": "DNCE-3oKIPusxB2uXOmoP0k",
-      "bitly_gif_url": "https://gph.is/2of28YL",
-      "bitly_url": "https://gph.is/2of28YL",
-      "embed_url": "https://giphy.com/embed/3oKIPusxB2uXOmoP0k",
-      "username": "DNCE",
-      "source": "https://www.youtube.com/watch?v=J7it_g_gAY4",
-      "title": "Barking Nicki Minaj GIF by DNCE",
-      "rating": "g",
-      "content_url": "",
-      "source_tld": "www.youtube.com",
-      "source_post_url": "https://www.youtube.com/watch?v=J7it_g_gAY4",
-      "is_sticker": 0,
-      "import_datetime": "2017-03-31 01:33:10",
-      "trending_datetime": "2017-08-29 04:45:01",
+      "images": {
+        "downsized_large": {
+          "url": "https://giphy.com/gifs/DNCE-3oKIPusxB2uXOmoP0k"
+        }
+      },
     },
     {
       "type": "gif",
       "id": "wtqEhmpTMmjRK",
       "url": "https://giphy.com/gifs/puppy-puppies-gif-wtqEhmpTMmjRK",
-      "slug": "puppy-puppies-gif-wtqEhmpTMmjRK",
-      "bitly_gif_url": "https://gph.is/1O6eqre",
-      "bitly_url": "https://gph.is/1O6eqre",
-      "embed_url": "https://giphy.com/embed/wtqEhmpTMmjRK",
-      "username": "",
-      "source": "https://everyhtingslove.tumblr.com/post/111691078552",
-      "title": "dog GIF",
-      "rating": "g",
-      "content_url": "",
-      "source_tld": "everyhtingslove.tumblr.com",
-      "source_post_url": "https://everyhtingslove.tumblr.com/post/111691078552",
-      "is_sticker": 0,
-      "import_datetime": "2015-02-21 21:32:07",
-      "trending_datetime": "1970-01-01 00:00:00",
+      "images": {
+        "downsized_large": {
+          "url": "https://giphy.com/gifs/puppy-puppies-gif-wtqEhmpTMmjRK"
+        }
+      },
     }
   ]
 };
